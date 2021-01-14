224ca1d73186001df5db756b403b3dd4aa292413
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-14 04:54:35 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Terry";
+const luckyNumber = 7;
+// const behave like let variables, except they cannot be reassigned ie
+// once const name = "Terry"; when we do name = "Marcelina"; it will generate an error.
+
+// {} refrences to the value assigned to the const variable!
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>Your lucky number is {luckyNumber}</p>
+    <p>Your lucky number is {Math.floor(Math.random() * 7)}</p>
+  </div>,
+  document.getElementById("root")
+);
