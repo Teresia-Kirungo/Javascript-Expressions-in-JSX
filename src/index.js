e02a570476f30870c68b1127f7c7413cbf098f9b
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-14 05:36:03 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 // const behave like let variables, except they cannot be reassigned ie
 // once const name = "Terry"; when we do name = "Marcelina"; it will generate an error.
 // const name = "Terry";
@@ -23,7 +22,7 @@ const firstName = "Terry";
 const lastName = "King";
 const luckyNumber = 7;
 
-// {} refrences to the value assigned to the const variable! Allowing you to write jS Statements 
+// {} refrences to the value assigned to the const variable! Allowing you to write jS Statements
 //     <h1>Hello {name}!</h1>
 
 // render can only take one html element, so in order to wrap many HTML elements to appear as one, have all of them go inside a div
@@ -31,13 +30,11 @@ const luckyNumber = 7;
 ReactDOM.render(
   <div>
     <h1>Hello {firstName + " " + lastName}!</h1>
-    <p>Your lucky number is {luckyNumber}</p>
-    <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
+    <p>Your static lucky number is {luckyNumber}</p>
+    <p>Your random lucky number is {Math.floor(Math.random() * 10)}</p>
     <p>Created by Ms Teresia Kirung'o</p>
     <p>Copyright @2021</p>
-
   </div>,
   document.getElementById("root")
 );
 //Performing concatenation in JavaScript inside the render option {firstName + " " + lastName}
-
