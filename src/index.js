d024571441eff3a02755bbc698fcf5bde4de1b0c
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2021-01-14 05:35:09 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,16 +17,20 @@ import ReactDOM from "react-dom";
 
 // const behave like let variables, except they cannot be reassigned ie
 // once const name = "Terry"; when we do name = "Marcelina"; it will generate an error.
-const name = "Terry";
+// const name = "Terry";
+
+const firstName = "Terry";
+const lastName = "King";
 const luckyNumber = 7;
 
 // {} refrences to the value assigned to the const variable! Allowing you to write jS Statements 
+//     <h1>Hello {name}!</h1>
 
 // render can only take one html element, so in order to wrap many HTML elements to appear as one, have all of them go inside a div
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {firstName + " " + lastName}!</h1>
     <p>Your lucky number is {luckyNumber}</p>
     <p>Your lucky number is {Math.floor(Math.random() * 10)}</p>
     <p>Created by Ms Teresia Kirung'o</p>
@@ -35,3 +39,5 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+//Performing concatenation in JavaScript inside the render option {firstName + " " + lastName}
+
